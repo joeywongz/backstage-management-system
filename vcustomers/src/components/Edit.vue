a6ad96599c014ee9ec155a0834d8f798bcd9e5eb
wangchong <370318843@qq.com>
2019-10-29 18:12:24 +0800

    项目上传

---
+++ vcustomers/src/components/Edit.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="edit container">
+      <h1 class="page-header">编辑用户</h1>
+      <form v-on:submit="updateCustomer">
+        <div class="well">
+          <h4>用户信息</h4>
+          <div class="form-group">
+            <label>姓名</label>
+            <input type="text" class="form-control" placeholder="name" v-model="customer.name">
+          </div>
+          <div class="form-group">
+            <label>电话</label>
+            <input type="text" class="form-control" placeholder="tel" v-model="customer.tel">
+          </div>
+          <div class="form-group">
+            <label>邮箱</label>
+            <input type="text" class="form-control" placeholder="email" v-model="customer.email">
+          </div>
+          <div class="form-group">
+            <label>学历</label>
+            <input type="text" class="form-control" placeholder="education" v-model="customer.education">
+          </div>
+          <div class="form-group">
+            <label>毕业学校</label>
+            <input type="text" class="form-control" placeholder="graduationschool" v-model="customer.graduationschool">
+          </div>
+          <div class="form-group">
+            <label>职业</label>
+            <input type="text" class="form-control" placeholder="profession" v-model="customer.profession">
+          </div>
+          <div class="form-group">
+            <label>个人简介</label>
+            <textarea rows="10" class="form-control" v-model="customer.profile"></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">确认</button>
+        </div>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Edit',
+    data () {
+      return {
+        customer:{}
+      }
+    },
+    methods: {
+      fetchCustomer(id){
+        this.$http.get('http://localhost:3000/users/'+id)
+          .then(function(response){
+            console.log(response)
+              this.customer = response.body
+          })
+      },
+      updateCustomer(e){
+        if(!this.customer.name || !this.customer.tel || !this.customer.email){
+          alert("信息不完善，请补充完整")
+        }else{
+          let updateCustomer = {
+            name: this.customer.name,
+            tel: this.customer.tel,
+            email: this.customer.email,
+            education: this.customer.education,
+            graduationschool: this.customer.graduationschool,
+            profession: this.customer.profession,
+            profile: this.customer.profile
+          };
+          this.$http.put("http://localhost:3000/users"+this.$route.params.id, updateCustomer)
+            .then(function(response){
+              this.$router.push({path:"/", query:{alert: "用户信息更新成功"}})
+              e.preventDefault()
+          })
+        };
+        e.preventDefault()
+      },
+      created(){
+        this.fetchCustomer(this.$route.params.id)
+      }
+    },
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  
+  </style>
+  
